5814f8ca287bc861bd675a2405479c63af6a534b
Colby Chiang <cc2qe@server.fake>
2013-05-01 20:31:28 -0400

    still trying to figure out commits

--- coverageStats.py
+++ coverageStats.py
@@ -13,20 +13,60 @@ def mean(depthCount, length):
     sumCov = 0.0
     
     for d in depthCount:
-        sumCov += d * float(chromDepth[d])
+        sumCov += d * float(depthCount[d])
     mean = sumCov / length
-    
     return mean
+
+def median(depthCount):
+    median = float
+    length=sum(depthCount.values())
+    
+    # the number of bases through the region
+    # we've gone with median
+    p = 0
+    
+    #
+    halfway = float(length) / 2
+        
+    # a list of the depth numbers. Sort it from
+    # smallest to largest and start counting to
+    # halfway through the # of bases
+    depthList = list(depthCount)
+    depthList.sort()
+
+    # iterator i
+    i = 0
+    while p <= halfway:
+        # if p is exactly halfway, then we must
+        # take the mean of the two surrounding.
+        if p == halfway:
+            median = (depthList[i-1] + depthList[i]) / float(2)
+            return median
+    
+        # myDepth is the coverage depth
+        myDepth = depthList[i]
+
+        # depthCount[myDepth] is the number of bases
+        # that have depth of myDepth
+        p += depthCount[myDepth]
+        i += 1
+    median = depthList[i-1]
+    return median
 
 def coverageStats(bedfile):
     
-    currentChrom = None
+    # vars for storing the previous line's info
+    prevChrom = None
+    prevEnd = 0
 
+    # counters to hold the depth counts over the chromosome
+    # and the whole genome
     chromDepth = Counter()
     genomeDepth = Counter()
 
-    chromSize = 0
-    genomeSize = 0
+    # initialize the chrom and genome lengths to zero
+    chromLength = 0
+    genomeLength = 0
 
     # declare global vars for each bed line
     chrom = str
@@ -35,49 +75,51 @@ def coverageStats(bedfile):
     depth = float
     
     # print header
-    print '\t'.join(('chr', 'length', 'mean depth'))
+    print '\t'.join(('chr', 'length', 'meanDepth', 'medianDepth'))
 
     for line in bedfile:
         v = line.rstrip().split('\t')
-
         chrom = v[0]
+
+        # if you run into a new chrom, print the
+        # stats on the previous chrom
+        if chrom != prevChrom:
+            # first make sure you're not looking at the
+            # first chrom
+            if prevChrom != None:
+                # set the chrom length and iterate the genome length
+                # (assumes BED file has all positions in each chrom
+                # even if it's zero coverage
+                chromLength = prevEnd
+                genomeLength += prevEnd
+                print '\t'.join(map(str,(prevChrom, chromLength, mean(chromDepth, chromLength), median(chromDepth))))
+
+                # start a new counter
+                chromDepth=Counter()
+
+        # cast the start end, and depth
         (start, end) = map(int, v[1:3])
         depth = float(v[3])
-        
         span = (end-start)
 
         # add span to depth counts
         chromDepth[depth] += span
         genomeDepth[depth] += span
+        
+        # store the previous line's data
+        prevChrom = chrom
+        prevEnd = end
 
-        # print chrom, depth
-        if chrom != currentChrom:
-            if currentChrom != None:
-                
-                
-                sumCov = 0
-                chromSize = end
-                genomeSize += end
-                for d in chromDepth:
-                    sumCov += d * chromDepth[d]
-                print '\t'.join(map(str,(currentChrom, chromSize, (sumCov/chromSize))))
-            currentChrom = chrom
-            chromDepth=Counter()
 
     # print the last chromosome
-    sumCov = 0
-    chromSize = 0
-    for d in chromDepth:
-        sumCov += d * chromDepth[d]
-        chromSize = end
-    print '\t'.join(map(str,(currentChrom, chromSize, (sumCov/chromSize))))
+    chromLength = end
+    print '\t'.join(map(str,(chrom, chromLength, mean(chromDepth, chromLength), median(chromDepth))))
+
+
 
     # print the whole genome coverage statistics    
-    sumCov = 0
-    for d in genomeDepth:
-        sumCov += (d * genomeDepth[d])
-        genomeSize += genomeDepth[d]
-    print '\t'.join(map(str,('total', genomeSize, (sumCov/genomeSize))))
+    genomeLength += end
+    print '\t'.join(map(str,('total', genomeLength, mean(genomeDepth, genomeLength), median(genomeDepth))))
     return
 
 # --------------------------------------
