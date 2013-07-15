dd5ff051ee97d3af593da3c309c2d2480471d775
Colby Chiang <cc2qe@virginia.edu>
2013-07-15 10:28:45 -0400

    expanded example script

--- drawIdeogram/example.R
+++ drawIdeogram/example.R
@@ -1,7 +1,14 @@
 source('~/code/seque/drawIdeogram/src/drawIdeogram.R')
 
+## Human chromosome example
+# define the plot area
+plot(c(0, getChromLength(10, genome='hg19')), c(-2,2), type='n')
+# draw the ideogram for human chromosome 16
+drawIdeogram(10, genome="hg19")
+
+
+## Mouse chromosome example
 # define the plot area
 plot(c(0, getChromLength(4, genome='mm9')), c(-2,2), type='n')
-
 # draw the ideogram for mouse chromosome 4
 drawIdeogram(4, genome="mm9")
