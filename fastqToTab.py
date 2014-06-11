c4d57a406a2919afc59fd2b9e737ff0e3593d480
Colby Chiang <cc2qe@virginia.edu>
2014-06-11 10:34:34 -0400

    don't error with broken pipe

--- fastqToTab.py
+++ fastqToTab.py
@@ -72,4 +72,8 @@ def main():
 
 # initialize the script
 if __name__ == '__main__':
-    sys.exit(main())
+    try:
+        sys.exit(main())
+    except IOError, e:
+        if e.errno != 32:  # ignore SIGPIPE
+            raise
